e69aa4fb293e676a845b232ec985dc012b9ae5f6
Howie <howie_howerton@trendmicro.com>
2019-05-09 11:58:14 -0400

    Update flask-docker.py

--- flask-docker.py
+++ flask-docker.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Holy crap!  It works!!'
+    return '<h1><center>Holy crap!  It works!!</center></h1>'
 
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0')
