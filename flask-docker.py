e933ce458466db28ff1d627e3f567f4c6096409f
Howie <howie_howerton@trendmicro.com>
2019-05-09 09:07:43 -0400

    Update flask-docker.py

--- flask-docker.py
+++ flask-docker.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'GitHub Webhook triggered the build!'
+    return 'Holy crap!  It\'s working!!'
 
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0')
